0e8c0dc0d50924171ab29aae16183f3bd8aa35b8
alexandre auriol <auriol.alexandre@gmail.com>
2019-11-12 17:17:00 +0100

    algo

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,4 +8,19 @@ class BookingsController < ApplicationController
     Booking.import(params[:file])
     redirect_to root_url, notice: "CSV importé"
   end
+
+  def admindata
+    memory = []
+    hash = {}
+    @hash = hash
+    Booking.all.each { |booking| hash[booking.representation].nil? ? hash[booking.representation] = [booking.prix] : hash[booking.representation] << booking.prix }
+    @representations = hash.map { |k, v| [k, v.sum.fdiv(v.length)] }
+    @bookings = []
+    Booking.where.not(age: nil).each do |booking|
+      unless memory.include?(booking.nom)
+        @bookings << booking
+        memory << booking.nom
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     collection {post :import}
   end
 root to: "bookings#index"
+get 'admindata', to: 'bookings#admindata', as: :admindata
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
